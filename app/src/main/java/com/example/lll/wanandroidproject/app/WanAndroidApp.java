a249b52d9e5be387a7cf83fad0319b0f7716dda4
lizhe998 <125956902@qq.com>
2019-03-24 23:52:14 +0800

    app

---
+++ app/src/main/java/com/example/lll/wanandroidproject/app/WanAndroidApp.java
@@ -0,0 +1,11 @@
+package com.example.lll.wanandroidproject.app;
+
+import android.app.Application;
+
+public class WanAndroidApp extends Application  {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
