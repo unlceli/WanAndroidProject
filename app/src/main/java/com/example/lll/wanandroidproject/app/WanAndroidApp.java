c5dd36009f3e7c7c9f53d6b03112ef4c749a0021
lizhe998 <125956902@qq.com>
2019-03-25 23:39:28 +0800

    app

--- app/src/main/java/com/example/lll/wanandroidproject/app/WanAndroidApp.java
+++ app/src/main/java/com/example/lll/wanandroidproject/app/WanAndroidApp.java
@@ -1,11 +1,23 @@
 package com.example.lll.wanandroidproject.app;
 
 import android.app.Application;
+import android.support.v7.app.AppCompatDelegate;
 
 public class WanAndroidApp extends Application  {
 
+
+    private static WanAndroidApp instance;
+
+    static {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+    }
     @Override
     public void onCreate() {
         super.onCreate();
+        instance =this;
+    }
+
+    public static synchronized WanAndroidApp getInstance(){
+        return instance;
     }
 }
