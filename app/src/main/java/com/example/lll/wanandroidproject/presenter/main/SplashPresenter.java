108d36dca8a53cf5115fe3523d7f7ac538ddc12e
lizhe998 <125956902@qq.com>
2019-04-11 23:42:16 +0800

    app

--- app/src/main/java/com/example/lll/wanandroidproject/presenter/main/SplashPresenter.java
+++ app/src/main/java/com/example/lll/wanandroidproject/presenter/main/SplashPresenter.java
@@ -4,7 +4,12 @@ import com.example.lll.wanandroidproject.base.presenter.BasePresenter;
 import com.example.lll.wanandroidproject.contract.main.SplashContract;
 import com.example.lll.wanandroidproject.core.DataManager;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.inject.Inject;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 
 public class SplashPresenter extends BasePresenter<SplashContract.View> implements SplashContract.Presenter {
 
@@ -16,6 +21,9 @@ public class SplashPresenter extends BasePresenter<SplashContract.View> implemen
     @Override
     public void attachView(SplashContract.View view) {
         super.attachView(view);
-
+        long splashTime = 2000;
+        addSubScribe(Observable.timer(splashTime, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(aLong -> view.jumpToMain()));
     }
 }
