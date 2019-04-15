7b1725b1dd50f80749a150b4c233a2663c822de7
lizhe998 <125956902@qq.com>
2019-04-15 23:29:18 +0800

    app

--- app/src/main/java/com/example/lll/wanandroidproject/ui/main/SplashActivity.java
+++ app/src/main/java/com/example/lll/wanandroidproject/ui/main/SplashActivity.java
@@ -1,11 +1,36 @@
 package com.example.lll.wanandroidproject.ui.main;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.example.lll.wanandroidproject.R;
 import com.example.lll.wanandroidproject.base.activity.BaseActivity;
 import com.example.lll.wanandroidproject.contract.main.SplashContract;
 import com.example.lll.wanandroidproject.presenter.main.SplashPresenter;
 
+import butterknife.BindView;
+
 public class SplashActivity extends BaseActivity<SplashPresenter> implements SplashContract.View {
+
+    @BindView(R.id.one_animation)
+    LottieAnimationView mOneAnimation;
+    @BindView(R.id.two_animation)
+    LottieAnimationView mTwoAnimation;
+    @BindView(R.id.three_animation)
+    LottieAnimationView mThreeAnimation;
+    @BindView(R.id.four_animation)
+    LottieAnimationView mFourAnimation;
+    @BindView(R.id.five_animation)
+    LottieAnimationView mFiveAnimation;
+    @BindView(R.id.six_animation)
+    LottieAnimationView mSixAnimation;
+    @BindView(R.id.seven_animation)
+    LottieAnimationView mSevenAnimation;
+    @BindView(R.id.eight_animation)
+    LottieAnimationView mEightAnimation;
+    @BindView(R.id.nine_animation)
+    LottieAnimationView mNineAnimation;
+    @BindView(R.id.ten_animation)
+    LottieAnimationView mTenAnimation;
+
     @Override
     protected int getLayoutId() {
         return R.layout.activity_splash;
