f0fd768146bc6c377717e4605461cd9099c63e32
lijj <125956902@qq.com>
2019-04-18 17:38:00 +0800

    欢迎界面

--- app/src/main/java/com/example/lll/wanandroidproject/core/dao/DaoSession.java
+++ app/src/main/java/com/example/lll/wanandroidproject/core/dao/DaoSession.java
@@ -1,5 +1,8 @@
 package com.example.lll.wanandroidproject.core.dao;
 
+
+
+
 import org.greenrobot.greendao.AbstractDao;
 import org.greenrobot.greendao.AbstractDaoSession;
 import org.greenrobot.greendao.database.Database;
@@ -8,15 +11,17 @@ import org.greenrobot.greendao.internal.DaoConfig;
 
 import java.util.Map;
 
+import json.chao.com.wanandroid.core.dao.HistoryDataDao;
+
 public class DaoSession extends AbstractDaoSession {
 
-    private final DaoConfig historyDataDaoConfig;
-    private final HistoryDataDao histroyDataDao;
+   // private final DaoConfig historyDataDaoConfig;
+   // private final HistoryDataDao histroyDataDao;
     public DaoSession(Database db, IdentityScopeType type, Map<Class<? extends AbstractDao<?,?>>,DaoConfig> daoConfigMap) {
         super(db);
-        historyDataDaoConfig =daoConfigMap.get(HistoryDataDao.class).clone();
-        historyDataDaoConfig.initIdentityScope(type);
-        histroyDataDao =new HistoryDataDao(historyDataDaoConfig,this);
-        new HistoryDataDao(historyDataDaoConfig,this);
+//        historyDataDaoConfig =daoConfigMap.get(HistoryDataDao.class).clone();
+//        historyDataDaoConfig.initIdentityScope(type);
+//        histroyDataDao =new HistoryDataDao(historyDataDaoConfig,this);
+//        new HistoryDataDao(historyDataDaoConfig,this);
     }
 }
